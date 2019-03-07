fd79b218bcae137dcdd5ce1bb6317e0140f6e965
shensheng <shensheng@31huiyi.com>
2019-03-07 11:28:17 +0800

    feat: 添加前端资源

---
+++ build/precache-manifest.744239f3f8f10bb55070f9c394a2e0bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "14926ac2c088a331b24d",
+    "url": "/static/js/main.14926ac2.chunk.js"
+  },
+  {
+    "revision": "a9882fcaa16f636d02af",
+    "url": "/static/js/1.a9882fca.chunk.js"
+  },
+  {
+    "revision": "14926ac2c088a331b24d",
+    "url": "/static/css/main.be06d55b.chunk.css"
+  },
+  {
+    "revision": "8ffe10edbc1f790774279275fb9daff1",
+    "url": "/index.html"
+  }
+];
