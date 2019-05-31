31fcfc44e79965de021eb13de818b363702630bf
shensheng <shensheng@31huiyi.com>
2019-05-31 09:49:11 +0800

    feat: 添加模版引擎

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { tokenHandle } from './common/tokenHandle'
 
 var app = express();
 
+app.set('view engine', 'html')
 app.use(express.static(resolveApp('build')))
 app.use(bodyParser.json())
 const init = async () => {
