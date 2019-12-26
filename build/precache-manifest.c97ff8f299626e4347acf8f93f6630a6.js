08d7ce8e4f6507b2fb40adaeb8c0ad5fde2fcfc5
shensheng <ss707494@163.com>
2019-12-26 18:09:47 +0800

    feat: 补货管理 build

---
+++ build/precache-manifest.c97ff8f299626e4347acf8f93f6630a6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d828ed4aaee7b04d09986bdff22bba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a8738e94daabfad108",
+    "url": "/static/css/4.b31142d4.chunk.css"
+  },
+  {
+    "revision": "3b44b00119432d627318",
+    "url": "/static/css/main.9f026f01.chunk.css"
+  },
+  {
+    "revision": "7cfcce862da87f81bae2",
+    "url": "/static/js/0.8b1e6bee.chunk.js"
+  },
+  {
+    "revision": "fb225955203ac984e2c2",
+    "url": "/static/js/1.9f6ead9e.chunk.js"
+  },
+  {
+    "revision": "b3a8738e94daabfad108",
+    "url": "/static/js/4.110966dc.chunk.js"
+  },
+  {
+    "revision": "a5988f9893f157e1e29e",
+    "url": "/static/js/5.dd8f4d42.chunk.js"
+  },
+  {
+    "revision": "f4053a3051c0a0e5640a",
+    "url": "/static/js/6.6d935c58.chunk.js"
+  },
+  {
+    "revision": "cfa16fd6e7842dfec4bd",
+    "url": "/static/js/7.79f7ec7b.chunk.js"
+  },
+  {
+    "revision": "1343e29a5a7e8bc4e549",
+    "url": "/static/js/8.7f7f6807.chunk.js"
+  },
+  {
+    "revision": "3bb796b0debf44b07ae2",
+    "url": "/static/js/9.e5d131fe.chunk.js"
+  },
+  {
+    "revision": "3b44b00119432d627318",
+    "url": "/static/js/main.94c1c703.chunk.js"
+  },
+  {
+    "revision": "588f6dabc5128853930a",
+    "url": "/static/js/runtime~main.455dfb3c.js"
+  }
+]);
