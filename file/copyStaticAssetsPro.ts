1598288cdf2b91f9011e2f3f297c2fdbd5150722
shensheng <ss707494@163.com>
2020-01-22 11:17:38 +0800

    feat: 字典 配置管理

--- file/copyStaticAssetsPro.ts
+++ file/copyStaticAssetsPro.ts
@@ -3,5 +3,5 @@ import * as shell from "shelljs";
 
 shell.rm('-rf', 'dist/schema')
 
-shell.cp("-Rf", "src/schema", "dist");
+// shell.cp("-Rf", "src/schema", "dist");
 shell.cp("-Rf", "src/assets", "dist");
