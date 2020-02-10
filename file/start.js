955b2e1289adddb4db8833e60c1880f3d087ae0d
shensheng <ss707494@163.com>
2020-02-10 23:08:35 +0800

    feat: 字典 配置管理

--- file/start.js
+++ file/start.js
@@ -1,8 +1,11 @@
 
 var shelljs = require('shelljs')
 
-shelljs.exec('npm run tsBuild && npm run startTsPro')
+shelljs.exec('npm run tsBuild && npm run startTsPro', function(code, stdot, stderr) {
+  console.log('Exit code:', code);
+  console.log('Program output:', stdout);
+  console.log('Program stderr:', stderr);
+})
 
-console.log('error end')
 
 
