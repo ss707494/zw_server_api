7a98419a6e08b6ae156866f1259e754367966738
shensheng <ss707494@163.com>
2020-03-08 20:29:31 +0800

    bug fix

--- file/copyStaticAssets.ts
+++ file/copyStaticAssets.ts
@@ -1,5 +1,10 @@
 
 import * as shell from "shelljs";
+import fs from "fs"
+
+if (!fs.existsSync('dist/schema')) {
+  fs.mkdirSync('dist/schema')
+}
 
 shell.cp("-Rf", "src/**/*.graphql", "dist/schema");
 shell.cp("-Rf", "src/assets", "dist");
