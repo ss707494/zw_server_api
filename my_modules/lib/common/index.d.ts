2095011edb97cffcdbf0d42239f435ee3949f2af
shensheng <ss707494@163.com>
2020-05-06 11:26:48 +0800

    修改 通用组件逻辑

--- my_modules/lib/common/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { PromoCodeTypeEnum } from './enum';
-export { PromoCodeTypeEnum };
-declare const _default: {};
-export default _default;
--- my_modules/lib/common/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var enum_1 = require("./enum");
-exports.PromoCodeTypeEnum = enum_1.PromoCodeTypeEnum;
-exports.default = {};
