d5424f63f3ea9d6db1802759e0cd71b903e7fc24
shensheng <ss707494@163.com>
2020-07-02 17:52:10 +0800

    deploy

--- client_build/server.js
+++ client_build/server.js
@@ -1,20 +1,7 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 const express = require('express')
-const cp = require('child_process')
-const { parse } = require('url')
 const next = require('next')
 
-const path = require('path')
-const setEnv = require('dotenv')
-;['.env.local', '.env'].forEach(e => {
-  setEnv.config({
-    path: path.resolve(process.cwd(), e)
-  })
-})
-
-const port = parseInt(process.env.PORT, 10) || 3006
-// console.log('process.env.NODE_ENV')
-// console.log(process.env.img_domain)
 // 判断开发环境和生产环境
 process.env.NODE_ENV = (typeof process.env.NODE_ENV !== 'undefined')
     ? process.env.NODE_ENV.trim()
@@ -27,6 +14,10 @@ const handle = app.getRequestHandler()
 
 app.prepare()
     .then(() => {
+      const port = parseInt(process.env.PORT, 10) || 3006
+// console.log('process.env.NODE_ENV')
+// console.log(process.env.img_domain)
+
       const server = express()
 
       server.use(
@@ -40,6 +31,7 @@ app.prepare()
       server.get('*', (req, res) => {
         return handle(req, res)
       })
+      console.log(process.env.client_api_uri)
 
       server.listen(port, (err) => {
         if (err) throw err
