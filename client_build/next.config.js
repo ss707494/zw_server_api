cc12812386801c8caba38f7a6b1bb45b5c45c004
shensheng <ss707494@163.com>
2020-07-02 15:28:29 +0800

    deploy

---
+++ client_build/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  publicRuntimeConfig: {
+    ss: '123',
+    imgDomain: process.env.img_domain,
+    client_api_uri: process.env.client_api_uri,
+    server_host: process.env.server_host,
+  },
+}
