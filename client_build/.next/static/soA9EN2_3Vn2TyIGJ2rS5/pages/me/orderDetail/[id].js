1affbc4c257bf4a2cf9e41c670effdb32d2f1d28
shensheng <ss707494@163.com>
2020-07-06 18:41:37 +0800

    拼团

--- client_build/.next/static/soA9EN2_3Vn2TyIGJ2rS5/pages/me/orderDetail/[id].js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[41],{ak1f:function(n,i,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/me/orderDetail/[id]",function(){return o("fbkJ")}])},fbkJ:function(n,i,o){"use strict";o.r(i);var w=o("/PM6");i.default=w.a}},[["ak1f",1,0,5,2,3,4,6,7,8,10,16]]]);
