6068bb5907acd95167ae4a7192d9c9bd070d3140
shensheng <ss707494@163.com>
2020-10-19 11:37:32 +0800

    fix bug

--- src/type_graphql/resolver/promoCode/promoCode.ts
+++ src/type_graphql/resolver/promoCode/promoCode.ts
@@ -2,7 +2,7 @@ import {Arg, Authorized, Ctx, Query, Resolver} from 'type-graphql'
 import {PromoCode} from '../../../entity/PromoCode'
 import {getRepository, Like} from 'typeorm'
 import {ContextType} from '../../apploServer'
-import {MoreThanOrEqual, Raw} from 'typeorm/index'
+import {MoreThanOrEqual, Raw} from 'typeorm'
 import {Dict} from '../../../entity/Dict'
 import {DictTypeEnum, PromoCodeTypeEnum} from '../../../common/ss_common/enum'
 import {User} from '../../../entity/User'
@@ -21,12 +21,13 @@ export class PromoCodeResolver {
           } || {}),
           promoCodeType: promoCodeItemInput?.promoCodeType ?? Like('%%'),
           code: promoCodeItemInput?.code,
+          ...([0, 1].includes(promoCodeItemInput?.isDisable) ? {isDisable: promoCodeItemInput?.isDisable} : {}),
           isDelete: 0,
-          effectiveDateStart: Raw(alias =>`${alias} < NOW()`),
-          effectiveDateEnd: Raw(alias =>`${alias} > NOW()`),
+          effectiveDateStart: Raw(alias => `${alias} < NOW()`),
+          effectiveDateEnd: Raw(alias => `${alias} > NOW()`),
         },
         relations: {
-          userLevel: true
+          userLevel: true,
         },
       })
     } else {
@@ -47,21 +48,22 @@ export class PromoCodeResolver {
           })
 
       return [
-          ...await getRepository(PromoCode).find({
-            where: {
-              ...(promoCodeItemInput?.title && {
-                title: Like(`%${promoCodeItemInput?.title}%`),
-              } || {}),
-              promoCodeType: PromoCodeTypeEnum.PromoCode,
-              code: promoCodeItemInput?.code,
-              isDelete: 0,
-              effectiveDateStart: Raw(alias =>`${alias} < NOW()`),
-              effectiveDateEnd: Raw(alias =>`${alias} > NOW()`),
-            },
-            relations: {
-              userLevel: true
-            },
-          }),
+        ...await getRepository(PromoCode).find({
+          where: {
+            ...(promoCodeItemInput?.title && {
+              title: Like(`%${promoCodeItemInput?.title}%`),
+            } || {}),
+            promoCodeType: PromoCodeTypeEnum.PromoCode,
+            code: promoCodeItemInput?.code,
+            isDelete: 0,
+            ...([0, 1].includes(promoCodeItemInput?.isDisable) ? {isDisable: promoCodeItemInput?.isDisable} : {}),
+            effectiveDateStart: Raw(alias => `${alias} < NOW()`),
+            effectiveDateEnd: Raw(alias => `${alias} > NOW()`),
+          },
+          relations: {
+            userLevel: true,
+          },
+        }),
         ...await getRepository(PromoCode).find({
           where: {
             userLevel: {
@@ -73,11 +75,11 @@ export class PromoCodeResolver {
             promoCodeType: PromoCodeTypeEnum.DarenCard,
             code: promoCodeItemInput?.code,
             isDelete: 0,
-            effectiveDateStart: Raw(alias =>`${alias} < NOW()`),
-            effectiveDateEnd: Raw(alias =>`${alias} > NOW()`),
+            effectiveDateStart: Raw(alias => `${alias} < NOW()`),
+            effectiveDateEnd: Raw(alias => `${alias} > NOW()`),
           },
           relations: {
-            userLevel: true
+            userLevel: true,
           },
         }),
       ]
